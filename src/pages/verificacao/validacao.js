9805a901f74660c0f219b419ec005f86258ff6ea
Junior Reis <gilmaikjunior@gmail.com>
2024-04-10 18:33:34 -0300

    Picking up value from the user

---
+++ src/pages/verificacao/validacao.js
@@ -0,0 +1,11 @@
+const check = document.getElementById("check")
+
+check.addEventListener('click', (stop)=>{
+stop.preventDefault
+
+let drug = document.getElementById("drug").value
+let Health = document.getElementById("Health").value
+let idade = document.getElementById("idade").value
+let weight = document.getElementById("weight").value
+
+})
