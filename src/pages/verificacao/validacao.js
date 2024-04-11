a3ae775af2c9cda448cab70f875bb47456d5ac5b
Junior Reis <gilmaikjunior@gmail.com>
2024-04-10 22:47:11 -0300

    add classname: Positiveresponse and NegativeAnswer

--- src/pages/verificacao/validacao.js
+++ src/pages/verificacao/validacao.js
@@ -1,11 +1,30 @@
-const check = document.getElementById("check")
+const check = document.getElementById("check");
+const divAnswer = document.getElementById("divAnswer");
+let respostaElement = null;
 
-check.addEventListener('click', (stop)=>{
-stop.preventDefault
+check.addEventListener('click', (event) => {
+    event.preventDefault();
 
-let drug = document.getElementById("drug").value
-let Health = document.getElementById("Health").value
-let idade = document.getElementById("idade").value
-let weight = document.getElementById("weight").value
+    let drug = document.getElementById("drug").value;
+    let health = document.getElementById("health").value;
+    let age = parseInt(document.getElementById("age").value);
+    let weight = parseFloat(document.getElementById("weight").value);
 
-})
+    verify(drug, health, age, weight);
+});
+
+function verify(drug, health, age, weight) {
+    // Se já houver uma resposta, atualiza-a, caso contrário, cria uma nova
+    if (respostaElement === null) {
+        respostaElement = document.createElement('p');
+        divAnswer.appendChild(respostaElement);
+    }
+
+    if (drug === 'nao' && health === 'sim' && age >= 18 && age <= 69 && weight >= 50) {
+        respostaElement.innerHTML = 'Parabéns, você pode doar!';
+        respostaElement.className = 'Positiveresponse';
+    } else {
+        respostaElement.innerHTML = 'Ops..., você não pode doar.';
+        respostaElement.className = 'NegativeAnswer';
+    }
+}
