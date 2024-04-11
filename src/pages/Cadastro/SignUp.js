4bcdcbf9d4ae79a668412cbd241184a8ce45c177
Junior Reis <gilmaikjunior@gmail.com>
2024-04-10 23:52:50 -0300

    add: Js

---
+++ src/pages/Cadastro/SignUp.js
@@ -0,0 +1,7 @@
+const SignUp = document.getElementById("SignUp")
+
+SignUp.addEventListener('click', ()=>{
+    const name = document.getElementById("name").value
+alert("Cadastro feito! Bem vindo: " + name )
+
+})
